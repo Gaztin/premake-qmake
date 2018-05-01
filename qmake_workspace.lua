d720e8bad4fdcde5ad4f5efdfc5bab6c163baae1
Sebastian Kylander <sebastian@kylander.me>
2018-05-01 17:44:11 +0200

    Write project info in subdirs project (workspace) file

--- qmake_workspace.lua
+++ qmake_workspace.lua
@@ -7,6 +7,42 @@ local m         = qmake.workspace
 --
 -- Generate a qmake workspace
 --
-function m.generate(prj)
+function m.generate(wks)
 	p.utf8()
+
+	m.template()
+	m.subprojects(wks)
+
+	m.subdirs(wks)
+end
+
+--
+-- Project type
+--
+function m.template()
+	p.out('TEMPLATE = subdirs\n\n')
+end
+
+--
+-- Sub-project names
+--
+function m.subprojects(wks)
+	p.out('SUBDIRS =')
+
+	for prj in p.workspace.eachproject(wks) do
+		p.w(' \\\n\t%s', prj.name)
+	end
+
+	p.out('\n\n')
+end
+
+--
+-- Locations of sub-projects
+--
+function m.subdirs(wks)
+	for prj in p.workspace.eachproject(wks) do
+		p.w('%s.subdir = %s/%s\n', prj.name, prj.location, prj.name)
+	end
+
+	p.out('\n')
 end
