069941194af1ab6e0531c93a5b24188e3ae39902
Sebastian Kylander <sebastian@kylander.me>
2018-05-27 16:43:00 +0200

    Add utility function for concatenating config names

--- qmake.lua
+++ qmake.lua
@@ -2,3 +2,18 @@
 include("_preload.lua")
 include("qmake_project.lua")
 include("qmake_workspace.lua")
+
+local p     = premake
+local qmake = p.extensions.qmake
+
+--
+-- Utility functions
+--
+
+function qmake.config(cfg)
+	if cfg.platform then
+		return cfg.platform .. ":" .. cfg.buildcfg
+	else
+		return cfg.buildcfg
+	end
+end
